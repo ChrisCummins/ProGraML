b46918dd6dfb38e6213ec447a7549080a1e696e8
Chris Cummins <chrisc.101@gmail.com>
2018-12-12 10:14:32 +0000

    Enable support for Colaboratory notebooks.

--- notebooks/notebooks.py
+++ notebooks/notebooks.py
@@ -1,22 +1,42 @@
 """Bazel-compatible wrapper around a Jupyter notebook server.
 
-This script launches a Jupyter notebook server. It never terminates. See
-//notebooks/BUILD for usage instructions.
+This script launches a Jupyter notebook server. It never terminates. It should
+be executed using the run_notebooks.sh helper script so that it can open and
+save files within the project source tree.
 """
+import typing
+
 from absl import app
 from absl import flags
+from absl import logging
 from notebook import notebookapp
 
 
 FLAGS = flags.FLAGS
 
+flags.DEFINE_bool('with_colaboratory', True,
+                  'Allow server to be used as a local runtime for Google '
+                  'Colaboratory notebooks.')
 
-def main(argv):
+
+def main(argv: typing.List[str]):
   """Main entry point."""
-  # TODO(cec): Pass start up options argv to notebookapp.main().
-  if len(argv) > 1:
-    raise app.UsageError("Unknown arguments: '{}'.".format(' '.join(argv[1:])))
-  notebookapp.main()
+  options = []
+
+  # Optionally enable server to be used as a local runtime for Google
+  # Colaboratory.
+  if FLAGS.with_colaboratory:
+    options += [
+      "--NotebookApp.nbserver_extensions={'jupyter_http_over_ws':True}",
+      "--NotebookApp.allow_origin='https://colab.research.google.com'",
+      '--NotebookApp.port_retries=0',
+    ]
+
+  # Append any arguments not parsed by absl.
+  options += argv[1:]
+
+  logging.info('Starting Jupyter notebook server with options: %s', options)
+  notebookapp.main(options)
 
 
 if __name__ == '__main__':
