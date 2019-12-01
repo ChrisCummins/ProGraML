e766a04373e09388e9393806bd5341e2e72fde82
Chris Cummins <chrisc.101@gmail.com>
2019-12-01 11:00:22 +0000

    Add a module for long-running job progress.

---
+++ labm8/py/progress.py
@@ -0,0 +1,109 @@
+"""This module aims to help evaluate the progress of long-running jobs."""
+import threading
+from typing import Callable
+from typing import Optional
+from typing import Union
+
+import tqdm
+
+from labm8.py import app
+from labm8.py import prof
+
+FLAGS = app.FLAGS
+
+
+class Progress(threading.Thread):
+  """A job with intermediate progress and a progress bar."""
+
+  def __init__(self, *args, **kwargs):
+    """Instantiate a long-running job.
+
+    Args:
+      args: Arguments for ProgressContext().
+      kwargs: Keyword arguments for ProgressContext().
+    """
+    self.ctx = ProgressContext(*args, **kwargs)
+    super(Progress, self).__init__()
+
+    self.run = self.Run
+    self.Start = self.start
+    self.IsAlive = self.is_alive
+
+  def Run(self):
+    """The run method. This should progress from [self.ctx.i, self.ctx.n]."""
+    raise NotImplementedError("abstract class")
+
+
+def Run(progress: Progress, refresh_time: float = 0.2) -> Progress:
+  """Run the given progress job until completion, updating the progress bar."""
+  progress.Start()
+  while progress.is_alive():
+    progress.ctx.Refresh()
+    progress.join(refresh_time)
+  progress.ctx.Refresh()
+  return progress
+
+
+class ProgressContext(object):
+  """The context for logging and profiling with a progress bar."""
+
+  def __init__(
+    self,
+    name: str,
+    i: int,
+    n: Optional[int] = None,
+    unit: str = "",
+    vertical_position: int = 0,
+  ):
+    """Construct a new progress.
+
+    Args:
+      name: The name of the job being processed. This is a prefix for the
+        progress bar output.
+      i: The starting value for the job.
+      n: The end value for the job. If not known this may be None. When None,
+        no estimated time or "bar" is printed during progress.
+      unit: An optional unit for progress bar.
+      vertical_position: The vertical position of the progress bar. Use this
+        when there are multiple progress bar concurrently.
+    """
+    self.name = name
+    self.i = i
+    self.n = n
+    self.unit = unit
+    self.vertical_position = vertical_position
+
+    # Prepend whitespace to the unit
+    unit = f" {unit}" if unit else unit
+
+    # Create the progress bar.
+    self.bar = tqdm.tqdm(
+      desc=self.name,
+      initial=self.i,
+      total=self.n,
+      unit=unit,
+      position=self.vertical_position,
+    )
+    self.print_context = self.bar.external_write_mode
+
+  def Refresh(self):
+    """Refresh the progress bar."""
+    self.bar.n = self.i
+    self.bar.refresh()
+
+  @app.skip_log_prefix
+  def Log(self, *args, **kwargs):
+    """Log a message."""
+    app.Log(*args, **kwargs, print_context=self.print_context)
+
+  def Warning(self, *args, **kwargs):
+    """Log a warning."""
+    app.Warning(*args, **kwargs, print_context=self.print_context)
+
+  def Error(self, *args, **kwargs):
+    """Log an error."""
+    app.Error(*args, **kwargs, print_context=self.print_context)
+
+  def Profile(self, level: int, msg: Union[str, Callable[[int], str]] = ""):
+    """Return a profiling context."""
+    return prof.Profile(msg, print_to=lambda x: self.Log(level, x),)
