fb1df015174ca3ad3c5647edf75b2753e3741bd3
Chris Cummins <chrisc.101@gmail.com>
2019-12-01 11:56:21 +0000

    Skip profile in logging context.

--- labm8/py/prof.py
+++ labm8/py/prof.py
@@ -143,7 +143,7 @@ def timers():
     yield name
 
 
-# @app.skip_log_prefix
+@app.skip_log_prefix
 @contextlib.contextmanager
 def Profile(
   name: typing.Union[str, typing.Callable[[int], str]] = "",
