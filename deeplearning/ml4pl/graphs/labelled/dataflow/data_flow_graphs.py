4c064fc755dd408c1bf7ee806e60dee3892dd73e
Chris Cummins <chrisc.101@gmail.com>
2019-12-19 20:13:43 +0000

    License //deeplearning/ml4pl/ under Apache v2.0.

--- deeplearning/ml4pl/graphs/labelled/dataflow/data_flow_graphs.py
+++ deeplearning/ml4pl/graphs/labelled/dataflow/data_flow_graphs.py
@@ -1,3 +1,18 @@
+# Copyright 2019 the ProGraML authors.
+#
+# Contact Chris Cummins <chrisc.101@gmail.com>.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 """This module defines a base class for implementing data flow analyses over
 networkx graphs.
 """
@@ -10,6 +25,7 @@ import networkx as nx
 from deeplearning.ml4pl.graphs import programl
 from deeplearning.ml4pl.graphs import programl_pb2
 from labm8.py import app
+
 
 FLAGS = app.FLAGS
 
--- deeplearning/ml4pl/run_id.py
+++ deeplearning/ml4pl/run_id.py
@@ -1,3 +1,18 @@
+# Copyright 2019 the ProGraML authors.
+#
+# Contact Chris Cummins <chrisc.101@gmail.com>.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 """This module defines a RUN_ID variable that uniquely identifies a program
 invocation.
 
@@ -23,6 +38,7 @@ from deeplearning.ml4pl import filesystem_paths
 from labm8.py import app
 from labm8.py import fs
 from labm8.py import system
+
 
 FLAGS = app.FLAGS
 
