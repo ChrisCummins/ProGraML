551629b3dffdd6bd4818fae43c4b1618d80bb9b6
Chris Cummins <chrisc.101@gmail.com>
2020-01-07 15:21:51 +0000

    Add a //labm8/sh:app library.

---
+++ labm8/sh/app.sh
@@ -0,0 +1,49 @@
+# Copyright 2014-2019 Chris Cummins <chrisc.101@gmail.com>.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# =============================================================================
+# labm8/sh/app.sh - Main entry point for shell libraries and scripts.
+#
+# Copy the following verbatim into the header of any scripts:
+#
+#     # --- begin labm8 init ---
+#     f=phd/labm8/sh/app.sh
+#     source "${RUNFILES_DIR:-/dev/null}/$f" 2>/dev/null || \
+#       source "$(grep -sm1 "^$f " "${RUNFILES_MANIFEST_FILE:-/dev/null}" | cut -f2- -d' ')" 2>/dev/null || \
+#       source "$0.runfiles/$f" 2>/dev/null || \
+#       source "$(grep -sm1 "^$f " "$0.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
+#       source "$(grep -sm1 "^$f " "$0.exe.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
+#       { echo>&2 "ERROR: cannot find $f"; exit 1; }; f=
+#     --- begin app init ---
+# =============================================================================
+
+# --- begin runfiles.bash initialization v2 ---
+# Copy-pasted from the Bazel Bash runfiles library v2.
+f=bazel_tools/tools/bash/runfiles/runfiles.bash
+source "${RUNFILES_DIR:-/dev/null}/$f" 2>/dev/null || \
+   source "$(grep -sm1 "^$f " "${RUNFILES_MANIFEST_FILE:-/dev/null}" | cut -f2- -d' ')" 2>/dev/null || \
+   source "$0.runfiles/$f" 2>/dev/null || \
+   source "$(grep -sm1 "^$f " "$0.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
+   source "$(grep -sm1 "^$f " "$0.exe.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
+   { echo>&2 "ERROR: cannot find $f"; exit 1; }; f=
+# --- end runfiles.bash initialization v2 ---
+
+# Args:
+#   $1 (str): The path to the data, including the name of the workspace.
+#
+# Returns:
+#   An absolute path, or an empty string if the file does not exist.
+DataPath() {
+  rlocation $1
+}
