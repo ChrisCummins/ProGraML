215144bd8aa94e3353c9f1385260d5f05d83070c
Chris Cummins <chrisc.101@gmail.com>
2020-02-19 10:39:40 -0600

    third_party: Update maven URLs.
    From https://gerrit.googlesource.com/bazlets/+/refs/heads/stable-2.15/tools/util.py
    
    Signed-off-by: format 2020.01.21 <github.com/ChrisCummins/format>

--- third_party/bazel/util.py
+++ third_party/bazel/util.py
@@ -14,9 +14,9 @@
 from os import path
 
 REPO_ROOTS = {
-  "GERRIT": "http://gerrit-maven.storage.googleapis.com",
+  "GERRIT": "https://gerrit-maven.storage.googleapis.com",
   "GERRIT_API": "https://gerrit-api.commondatastorage.googleapis.com/release",
-  "MAVEN_CENTRAL": "http://central.maven.org/maven2",
+  "MAVEN_CENTRAL": "https://repo1.maven.org/maven2",
   "MAVEN_LOCAL": "file://" + path.expanduser("~/.m2/repository"),
   "MAVEN_SNAPSHOT": "https://oss.sonatype.org/content/repositories/snapshots",
 }
