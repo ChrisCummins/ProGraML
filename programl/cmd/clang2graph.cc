fec380337ddc6c1796d5958a11e437773a677088
Chris Cummins <chrisc.101@gmail.com>
2020-06-11 11:14:23 +0100

    Use explicit endline character.
    std::endl is not supported by llvm::errs().
    
    Signed-off-by: format 2020.06.15 <github.com/ChrisCummins/format>

--- programl/cmd/clang2graph.cc
+++ programl/cmd/clang2graph.cc
@@ -166,7 +166,7 @@ int main(int argc, const char **argv, char *const *envp) {
       returncode = 0;
       std::cout << graph.DebugString();
     } else {
-      llvm::errs() << status.error_message() << std::endl;
+      llvm::errs() << status.error_message() << '\n';
     }
   }
 
